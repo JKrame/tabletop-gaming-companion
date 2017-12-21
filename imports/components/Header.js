c6acf7c8dacedd788489f849e18c69b33aab6b2a
JKramerDD <jkramerdesigns@hotmail.com>
2017-12-21 15:30:54 -0500

    Header styling

--- imports/components/Header.js
+++ imports/components/Header.js
@@ -13,15 +13,7 @@ export default class Header extends React.Component{
         {
             return(
                 <header>
-                    <ul>
-                    <li><a>Home</a></li>
-                    <li><a>Adventure Board</a></li>
-                    <li><a>Binder</a></li>
-                    <li><a>Mail</a></li>
-                    <li><a>Settings</a></li>
-                    </ul>
-    
-                    <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+                    <nav className="navbar navbar-toggleable-md navbar-default">
                         <div className="container-fluid">
                             <div className="navbar-header">
                                 <button className="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#collapsibleNavbarContent" aria-controls="collapsibleNavbarContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,11 +21,18 @@ export default class Header extends React.Component{
                                     <span className="icon-bar"></span>
                                     <span className="icon-bar"></span>
                                 </button>
-                                <NavLink to='/' className='navbar-brand'><img src='images/primoHorizontal.png'/></NavLink>
+        
+                                <NavLink to='/' className='navbar-brand' style={{"margin-top":"-10px"}}><img src='images/primoHorizontal.png'/></NavLink>
+                              
                             </div>                
-                                
                             <div className="collapse navbar-collapse pull-right" id="collapsibleNavbarContent" >
-    
+                                <ul className="nav navbar-nav mr-auto">
+                                    <li ><a  href="#">Home</a></li>
+                                    <li ><a  href="#">Adventure Board</a></li>
+                                    <li ><a  href="#">Binder</a></li>
+                                    <li ><a  href="#">Mail</a></li>
+                                    <li ><a  href="#">Settings</a></li>
+                                </ul>
                             </div>
                         </div>
                     </nav>
