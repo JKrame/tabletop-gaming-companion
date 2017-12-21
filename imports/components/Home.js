5d53fc2cb585a90f0a7bf032460f1d3a7514b54a
JKramerDD <jkramerdesigns@hotmail.com>
2017-12-21 17:17:20 -0500

    Homepage Styling

--- imports/components/Home.js
+++ imports/components/Home.js
@@ -3,10 +3,42 @@ import React from 'react'
 export default class Home extends React.Component{
   render() {
     return(
-        <div>
-          <h1>Home </h1>
-          <div className="item"></div>
-        </div>
+      <div className="page-wrapper">
+          <div className="col-lg-8 col-lg-offset-2">
+                <div className="col-lg-6 ">
+                  <div className="page-content-half">
+                      <h3>Characters >></h3>
+                      <hr/>
+                      <br/>
+                      <div className="item"></div>
+                    </div>
+                </div>
+                <div className="col-lg-6 ">
+                <div className="page-content-half">
+                    <h3>Campaigns >></h3>
+                    <hr/>
+                    <br/>
+                    <div className="item"></div>
+                  </div>
+              </div>
+              <div className="col-lg-6 ">
+              <div className="page-content-half">
+                  <h3>Players Nearby >></h3>
+                  <hr/>
+                  <br/>
+                  <div className="item"></div>
+                </div>
+            </div>
+            <div className="col-lg-6 ">
+            <div className="page-content-half">
+                <h3>Meeting Spots >></h3>
+                <hr/>
+                <br/>
+                <div className="item"></div>
+              </div>
+          </div>
+          </div>
+      </div>
     );
   }
 }  
