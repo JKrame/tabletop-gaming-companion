5f60814e486380e4d025bd03b8e6b3b24f38ea67
Michael Garro <mgarro385@knights.ucf.edu>
2018-01-23 13:25:31 -0500

    Added CampaignCardHalf.js. Added renderCampaignCard and renderCharacterCard methods to Home.js component and Binder.js component. Both now dynamically call the method to render the card JSX. Still need props added to methods for dynamic values.

--- imports/components/Home.js
+++ imports/components/Home.js
@@ -1,172 +1,121 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { Characters } from '../api/character';
-
+import CharacterCardHalf from '../objects/CharacterCardHalf';
+import CampaignCardHalf from '../objects/CampaignCardHalf';
 
 export default class Home extends React.Component{
 
-  onSubmit(e){  
-    //gets the character name
-    const characterName = this.refs.characterName.value.trim();
+    onSubmit(e){  
+        //gets the character name
+        const characterName = this.refs.characterName.value.trim();
 
-    e.preventDefault();
+        e.preventDefault();
 
-    //checks if value exists
-    if (characterName) {
-      Characters.insert({ characterName });
-      this.refs.characterName.value = '';
+        //checks if value exists
+        if (characterName) {
+        Characters.insert({ characterName });
+        this.refs.characterName.value = '';
+        }
     }
-  }
 
+    renderCharacterCard() {
+        var cards = [];
+        var numcharacters = 4;
+        for (var i = 0; i < numcharacters; i++)
+        {
+            cards.push(<CharacterCardHalf/>);
+        }
+        return <div>{cards}</div>;
+    }
 
+    renderCampaignCard() {
+        var cards = [];
+        var numcampaigns = 2;
+        for (var i = 0; i < numcampaigns; i++)
+        {
+            cards.push(<CampaignCardHalf/>);
+        }
+        return <div>{cards}</div>;
+    }
 
-  render() {
-    return(
-      <div className="page-wrapper">
-          <div className="col-lg-8 col-lg-offset-2">
-                <div className="col-md-6 ">
-                  
-                  
-                  <div className="page-content-half">
-                      <h3>Characters >></h3>
-                      <hr/>
-                          <div className="page-content-scroller">
-                              <div className="objectCardHalf ">
-                                  <div className="objectCardHalfImage">
-                                      <img src={'/images/placeholder.jpg'}/>
-                                  </div>
-                                  <div className="objectCardHalfInfo container-fluid">
-                                      <h4>Elfman Needsaname</h4>
-                                      <hr className="hr-override container-fluid"/>
-                                      <p className="p-override"> Class: Druid</p>
-                                      <p className="p-override"> Level: 4</p>
-                                      <p className="p-override"> Race: Elf</p>
-                                  </div>
-                              </div>
-                              <div className="objectCardHalf ">
-                                  <div className="objectCardHalfImage">
-                                      <img src={'/images/placeholder.jpg'}/>
-                                  </div>
-                                  <div className="objectCardHalfInfo container-fluid">
-                                      <h4>Elfman Needsaname</h4>
-                                      <hr className="hr-override container-fluid"/>
-                                      <p className="p-override"> Class: Druid</p>
-                                      <p className="p-override"> Level: 4</p>
-                                      <p className="p-override"> Race: Elf</p>
-                                  </div>
-                              </div>
-                              <div className="objectCardHalf ">
-                                  <div className="objectCardHalfImage">
-                                      <img src={'/images/placeholder.jpg'}/>
-                                  </div>
-                                  <div className="objectCardHalfInfo container-fluid">
-                                      <h4>Elfman Needsaname</h4>
-                                      <hr className="hr-override container-fluid"/>
-                                      <p className="p-override"> Class: Druid</p>
-                                      <p className="p-override"> Level: 4</p>
-                                      <p className="p-override"> Race: Elf</p>
-                                  </div>
-                              </div>
-                              <div className="objectCardHalf ">
-                                  <div className="objectCardHalfImage">
-                                      <img src={'/images/placeholder.jpg'}/>
-                                  </div>
-                                  <div className="objectCardHalfInfo container-fluid">
-                                      <h4>Elfman Needsaname</h4>
-                                      <hr className="hr-override container-fluid"/>
-                                      <p className="p-override"> Class: Druid</p>
-                                      <p className="p-override"> Level: 4</p>
-                                      <p className="p-override"> Race: Elf</p>
-                                  </div>
-                              </div>
-                              
-                            <NavLink to='/character/edit/' className='nav-item nav-link'>              
-                                <div className="objectCardHalf ">
-                                    <div className="objectCardHalfImage">
-                                        <img src={'/images/addIcon.png'}/>
+    render() {
+        return(
+        <div className="page-wrapper">
+            <div className="col-lg-8 col-lg-offset-2">
+                    <div className="col-md-6 ">
+
+                    <div className="page-content-half">
+                        <h3>Characters >></h3>
+                        <hr/>
+                            <div className="page-content-scroller">
+                            {this.renderCharacterCard()}
+                                <NavLink to='/character/edit/' className='nav-item nav-link'>              
+                                    <div className="objectCardHalf ">
+                                        <div className="objectCardHalfImage">
+                                            <img src={'/images/addIcon.png'}/>
+                                        </div>
+                                        <div className="objectCardHalfInfo container-fluid">
+                                            <h4>CREATE NEW CHARACTER</h4>
+                                            <hr className="hr-override container-fluid"/>
+                                        </div>
                                     </div>
+                                </NavLink>
+                            </div>
+                        </div>
+                    </div>
+                    
+                <div className="col-md-6 ">
+                    <div className="page-content-half">
+                        <h3>Campaigns >></h3>
+                        <hr/>
+                        
+                        <div className="page-content-scroller">
+                            {this.renderCampaignCard()}
+                            <div className="objectCardHalf ">
+                                <div className="objectCardHalfImage">
+                                    <img src={'/images/pending.png'}/>
+                                </div>
+                                <div className="objectCardHalfInfo container-fluid">
+                                    <h4>PENDING INVITE</h4>
+                                    <hr className="hr-override container-fluid"/>
+                                    <p className="p-override">Click for Details...</p>
+                                </div>
+                            </div>
+
+                            <NavLink to='/campaign/edit/' className='nav-item nav-link'>                                          
+                                <div className="objectCardHalf ">
+                                        <div className="objectCardHalfImage">
+                                            <img src={'/images/addIcon.png'}/>
+                                        </div>
                                     <div className="objectCardHalfInfo container-fluid">
-                                        <h4>CREATE NEW CHARACTER</h4>
+                                        <h4>CREATE NEW CAMPAIGN</h4>
                                         <hr className="hr-override container-fluid"/>
                                     </div>
                                 </div>
                             </NavLink>
-                          </div>
-                      </div>
+                        </div>
+                        </div>
                 </div>
-                
-              <div className="col-md-6 ">
-                  <div className="page-content-half">
-                      <h3>Campaigns >></h3>
-                      <hr/>
-                      
-                      <div className="page-content-scroller">
-                          <div className="objectCardHalf ">
-                              <div className="objectCardHalfImage">
-                                  <img src={'/images/placeholder.jpg'}/>
-                              </div>
-                              <div className="objectCardHalfInfo container-fluid">
-                                  <h4>CampaignName</h4>
-                                  <hr className="hr-override container-fluid"/>
-                                  <p className="p-override"> Campaign Description...</p>
-                              </div>
-                          </div>
-                      
-                          <div className="objectCardHalf ">
-                              <div className="objectCardHalfImage">
-                                  <img src={'/images/placeholder.jpg'}/>
-                              </div>
-                              <div className="objectCardHalfInfo container-fluid">
-                                  <h4>CampaignName</h4>
-                                  <hr className="hr-override container-fluid"/>
-                                  <p className="p-override"> Campaign Description...</p>
-                              </div>
-                          </div>
-
-                          <div className="objectCardHalf ">
-                              <div className="objectCardHalfImage">
-                                  <img src={'/images/pending.png'}/>
-                              </div>
-                              <div className="objectCardHalfInfo container-fluid">
-                                  <h4>PENDING INVITE</h4>
-                                  <hr className="hr-override container-fluid"/>
-                                  <p className="p-override">Click for Details...</p>
-                              </div>
-                          </div>
-
-                        <NavLink to='/campaign/edit/' className='nav-item nav-link'>                                          
-                            <div className="objectCardHalf ">
-                                    <div className="objectCardHalfImage">
-                                        <img src={'/images/addIcon.png'}/>
-                                    </div>
-                                <div className="objectCardHalfInfo container-fluid">
-                                    <h4>CREATE NEW CAMPAIGN</h4>
-                                    <hr className="hr-override container-fluid"/>
-                                </div>
-                            </div>
-                        </NavLink>
-                      </div>
+                <div className="col-md-6 ">
+                <div className="page-content-half">
+                    <h3>Players Nearby >></h3>
+                    <hr/>
+                    <br/>
+                    <div className="item"></div>
                     </div>
-              </div>
-              <div className="col-md-6 ">
-              <div className="page-content-half">
-                  <h3>Players Nearby >></h3>
-                  <hr/>
-                  <br/>
-                  <div className="item"></div>
+                </div>
+                <div className="col-md-6 ">
+                <div className="page-content-half">
+                    <h3>Meeting Spots >></h3>
+                    <hr/>
+                    <br/>
+                    <div className="item"></div>
                 </div>
             </div>
-            <div className="col-md-6 ">
-            <div className="page-content-half">
-                <h3>Meeting Spots >></h3>
-                <hr/>
-                <br/>
-                <div className="item"></div>
-              </div>
-          </div>
-          </div>
-      </div>
-    );
-  }
+            </div>
+        </div>
+        );
+    }
 }  
--- imports/objects/CampaignCardHalf.js
+++ imports/objects/CampaignCardHalf.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default class CharacterCardHalf extends React.Component{
+export default class CampaignCardHalf extends React.Component{
     render() {
         return (
             <div className="objectCardHalf ">
@@ -8,11 +8,9 @@ export default class CharacterCardHalf extends React.Component{
                     <img src={'/images/placeholder.jpg'}/>
                 </div>
                 <div className="objectCardHalfInfo container-fluid">
-                    <h4>Elfman Needsaname</h4>
+                    <h4>CampaignName</h4>
                     <hr className="hr-override container-fluid"/>
-                    <p className="p-override"> Class: Druid</p>
-                    <p className="p-override"> Level: 4</p>
-                    <p className="p-override"> Race: Elf</p>
+                    <p className="p-override"> Campaign Description...</p>
                 </div>
             </div>
         );
