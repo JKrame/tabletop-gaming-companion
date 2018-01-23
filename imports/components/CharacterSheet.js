c996a3e77eeb793d4232c620daef4dba93b76f4f
JKramerDD <jkramerdesigns@hotmail.com>
2018-01-23 14:28:10 -0500

    Character Page Image

--- imports/components/CharacterSheet.js
+++ imports/components/CharacterSheet.js
@@ -27,7 +27,23 @@ export default class CharacterSheet extends React.Component{
                     <hr/>
                     
                     <div className="col-sm-4 split-page-left container">
+                        <img src={'/images/photoMissing.png'} className="full-width"/>
+                        <div className="spacer col-sm-12"/>
+
+                        <form>
+                            <div className="col-sm-12">
+                                <p className="p-override">IMAGE URL</p>
+                                <input className="full-width" type="text" ref="characterImageURL" placeholder=""/>
+                            </div>
+                            <div className="spacer col-sm-12"/>
+                            <div className="spacer col-sm-12"/>
+
+                            <div className="col-sm-12">
+                                <button className="full-width submit-button">UPDATE IMAGE   </button>
+                            </div>
+                        </form>
                     </div>
+                    
 
                     <div className="col-sm-8 split-page-right left-border container scrolling-container">
                         <CharacterForm />
