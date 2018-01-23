edb9ead935a87a2f4e12e64d0d1800cb7bd8a4ff
Michael Garro <mgarro385@knights.ucf.edu>
2018-01-23 11:49:42 -0500

    Created CharacterCardHalf.js

---
+++ imports/objects/CharacterCardHalf.js
@@ -0,0 +1,12 @@
+<div className="objectCardHalf ">
+    <div className="objectCardHalfImage">
+        <img src={'/images/placeholder.jpg'}/>
+    </div>
+    <div className="objectCardHalfInfo container-fluid">
+        <h4>Elfman Needsaname</h4>
+        <hr className="hr-override container-fluid"/>
+        <p className="p-override"> Class: Druid</p>
+        <p className="p-override"> Level: 4</p>
+        <p className="p-override"> Race: Elf</p>
+    </div>
+</div>
