f33ad5e42609ae8e4f6d74b23205eac4d97e0195
JKramerDD <jkramerdesigns@hotmail.com>
2018-01-25 12:16:58 -0500

    Adventure Board page

--- imports/components/AdventureBoard.js
+++ imports/components/AdventureBoard.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import {Random} from 'meteor/random';
+
+import CharacterCardHalf from '../objects/CharacterCardHalf';
+import CampaignCardHalf from '../objects/CampaignCardHalf';
+
 
 export default class AdventureBoard extends React.Component{
+    renderCampaignCard() {
+        var cards = [];
+        var numcampaigns = 20;
+        for (var i = 0; i < numcampaigns; i++)
+        {
+            cards.push(<CampaignCardHalf key={i}/>);
+        }
+        return <div>{cards}</div>;
+    }
   render() {
     return(
         <div className="page-wrapper">
-          <div className="col-lg-8 col-lg-offset-2">
-            <div className="page-content">
-              <h3>Adventure Board >></h3>
-              <hr/>
+                <div className="col-lg-8 col-lg-offset-2">
+                    <div className="page-content col-xs-12 fill-height" >
+                        <NavLink to="Characters">
+                                <h3>Public Adventure Board</h3>
+                        </NavLink>
+                        <hr/>
+                        <div className="scrolling-container">
+                            {this.renderCampaignCard()}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
     );
   }
 }  
