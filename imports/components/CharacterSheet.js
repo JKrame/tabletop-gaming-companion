8749b4f5257afed9f2adde42b283aacf7f232109
JKramerDD <jkramerdesigns@hotmail.com>
2018-02-01 12:19:52 -0500

    CharacterSheet Cleanup

--- imports/components/CharacterSheet.js
+++ imports/components/CharacterSheet.js
@@ -6,6 +6,7 @@ import CharacterForm from '../objects/CharacterForm';
 var character;
 var characterName;
 var cs;
+
 export default class CharacterSheet extends React.Component{
     constructor(props){
         super(props);
@@ -20,11 +21,11 @@ export default class CharacterSheet extends React.Component{
     }
 
     componentWillMount(){
-        this.setState({id: this.props.match.params._id});
+        id = this.props.match.params._id;
+        this.setState({id: id});
         console.log("cs > componentDidMount");
         this.characterSheetTracker = Tracker.autorun(() => {
             const sub = Meteor.subscribe('characters');
-            id = this.props.match.params._id;
             console.log("cs > componentDidMount > tracker");
             console.log(sub.ready());
             if(sub.ready())
@@ -68,28 +69,8 @@ export default class CharacterSheet extends React.Component{
                         <h3>Character Sheet >></h3>
                         <hr/>
                         
-                        <div className="col-sm-4 split-page-left container">
-                            <img src={this.renderImage()} className="full-width"/>
-                            <div className="spacer col-sm-12"/>
 
-                            <form>
-                                <div className="col-sm-12">
-                                    <p className="p-override">IMAGE URL</p>
-                                    <input className="full-width" type="text" ref="characterImageURL" placeholder={characterName != null ? characterName : ""}/>
-                                </div>
-                                <div className="spacer col-sm-12"/>
-                                <div className="spacer col-sm-12"/>
-
-                                <div className="col-sm-12">
-                                    <button className="full-width submit-button">UPDATE IMAGE   </button>
-                                </div>
-                            </form>
-                        </div>
-                        
-
-                        <div className="col-sm-8 split-page-right left-border container">
                             {this.renderForm()}
-                        </div>
                     </div>
                 </div>
             </div>
