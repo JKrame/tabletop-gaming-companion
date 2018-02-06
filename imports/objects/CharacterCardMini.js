77059689cef26853926150ee24d57bf51867507d
Austin Shipley <austin.r.shipley@gmail.com>
2018-02-06 14:43:23 -0500

    trying to add a delete button to charactercardmini

--- imports/objects/CharacterCardMini.js
+++ imports/objects/CharacterCardMini.js
@@ -2,21 +2,30 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 
 export default class CharacterCardMini extends React.Component{
+    deleteCharacter(){
+        Meteor.call('characters.remove', this.props.id);
+    }
+
     render() {
         return (
-            <NavLink to='#' onClick={() => this.props.func(this.props.id, this.props.somehistory)} className='nav-item nav-link'>
+            <div className='nav-item nav-link'>
                 <div className="objectCardMini highlight-container">
-                    <div className="objectCardMiniImage">
-                        <img src={'/images/placeholder.jpg'} className="stretch-image"/>
+                    <div onClick={() => this.props.func(this.props.id, this.props.somehistory)}>
+                        <div className="objectCardMiniImage">
+                            <img src={'/images/placeholder.jpg'} className="stretch-image"/>
+                        </div>
+                        <div className="objectCardMiniInfo container-fluid">
+                            <h5 className="no-margin-override h5-overflow-hidden">{this.props.characterName}</h5>
+                            <hr className="hr-override-light"/>
+                            <p className="p-override no-margin-override small-text"> Class: {this.props.characterClass}</p>
+                            <p className="p-override no-margin-override small-text"> Level: {this.props.level}</p>
+                        </div>
                     </div>
-                    <div className="objectCardMiniInfo container-fluid">
-                        <h5 className="no-margin-override h5-overflow-hidden">{this.props.characterName}</h5>
-                        <hr className="hr-override-light"/>
-                        <p className="p-override no-margin-override small-text"> Class: {this.props.characterClass}</p>
-                        <p className="p-override no-margin-override small-text"> Level: {this.props.level}</p>
+                    <div onClick={this.deleteCharacter}>
+                        <p>DELETE</p>
                     </div>
                 </div>
-            </NavLink>
+            </div>
         );
     }
 }
