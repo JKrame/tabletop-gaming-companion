001d29564fb810ebcc7577ede59b3dc1d356581b
Andrew Maida <agmaida@knights.ucf.edu>
2018-02-19 16:57:01 -0500

    some changes to settings page.  Meteor.user() is not loading as fast as it should, so more changes are incomming

--- imports/components/Settings.js
+++ imports/components/Settings.js
@@ -4,13 +4,61 @@ import { Geolocation } from 'meteor/mdg:geolocation';
 import { reverseGeocode } from 'meteor/jaymc:google-reverse-geocode';
 
 export default class Settings extends React.Component{
-    
+
     constructor(init){
         super(init);
-        this.handleChange = this.handleChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleScheduleChange = this.handleScheduleChange.bind(this);
     }
 
-    handleChange({target}){
+    componentWillMount(){
+        id = Meteor.userId();
+        console.log(id)
+        this.settingsTracker = Tracker.autorun(() => {
+            const sub = Meteor.subscribe('users');
+            if(sub.ready())
+            {
+                console.log("ready")
+                this.user = users.findOne({_id : this.id});               
+            }
+            this.forceUpdate();
+        });
+    }
+
+    componentWillUnmount(){
+        this.settingsTracker.stop();
+    }
+
+    onSubmit(e){  
+        e.preventDefault();
+        //console.log(this.refs.Monday.checked)
+        // if(this.refs.location.checked){
+        //     this.setLocation()
+        // }
+        schedule = [this.refs.Monday.checked, this.refs.Tuesday.checked, this.refs.Wednesday.checked, this.refs.Thursday.checked, this.refs.Friday.checked, this.refs.Saturday.checked, this.refs.Sunday.checked]
+        Meteor.users.update(Meteor.userId(), {
+            $set: {"profile.schedule": schedule}
+        });
+        console.log(schedule)
+    }
+
+
+
+
+    handleScheduleChange({target}){
+        if (target.checked){
+            Meteor.users.update(Meteor.userId(), {
+                $addToSet: {"profile.schedule": target.value}
+            });
+
+        } else{
+            Meteor.users.update(Meteor.userId(), {
+                $pull: {"profile.schedule": target.value}
+            });
+        }
+    }
+
+    handleLocationChange({target}){
         if (target.checked){
             this.setLocation.call()
             console.log("checked")
@@ -66,7 +114,7 @@ export default class Settings extends React.Component{
                     </div>
 
                     <div className="col-sm-8 split-page-right left-border container">
-                        <form >
+                        <form onSubmit={this.onSubmit.bind(this)}>
                             <div className="scrolling-container-smaller">
                                 <div className="col-sm-12">
                                     <p className="p-override">NAME</p>
@@ -75,24 +123,32 @@ export default class Settings extends React.Component{
                             <div className="spacer col-sm-12"/>
                                 <div className="col-sm-12">
                                     <p className="p-override">Location</p>
-                                    <input
-                                    id={this.id}
-                                    type="checkbox"
-                                    value="test"
-                                    ref="complete"
-                                    onChange={this.handleChange}
-                                    />
+                                    <input id={this.id} type="checkbox" value="test" ref="location"/>
                                     <label htmlFor={this.id}> Opt in</label>
                                 </div>                        
                             <div className="spacer col-sm-12"/>
                                 <div className="col-sm-12">
-                                    <p className="p-override">NAME</p>
-                                    <input className="full-width" type="text" ref="characterName" />
+                                    <p className="p-override">Available Game Days</p>
+                                    <input id={this.id} type="checkbox" defaultChecked={Meteor.user()? Meteor.user().profile.schedule[0] : false} value="Monday" ref="Monday" />
+                                    <label htmlFor={this.id}> Monday</label>
+                                    <input id={this.id} type="checkbox" defaultChecked={Meteor.user() ? Meteor.user().profile.schedule[1] : false} value="Tuesday" ref="Tuesday"/>
+                                    <label htmlFor={this.id}> Tuesday</label>
+                                    <input id={this.id} type="checkbox" defaultChecked={Meteor.user() ? Meteor.user().profile.schedule[2] : false} value="Wednesday" ref="Wednesday"/>
+                                    <label htmlFor={this.id}> Wednesday</label>
+                                    <input id={this.id} type="checkbox" defaultChecked={Meteor.user()? Meteor.user().profile.schedule[0] : false} value="Thursday" ref="Thursday"/>
+                                    <label htmlFor={this.id}> Thursday</label>
+                                    <input id={this.id} type="checkbox" defaultChecked={Meteor.user()? Meteor.user().profile.schedule[0] : false} value="Friday" ref="Friday"/>
+                                    <label htmlFor={this.id}> Friday</label>
+                                    <input id={this.id} type="checkbox" defaultChecked={Meteor.user()? Meteor.user().profile.schedule[0] : false} value="Saturday" ref="Saturday" />
+                                    <label htmlFor={this.id}> Saturday</label>
+                                    <input id={this.id} type="checkbox" defaultChecked={Meteor.user()? Meteor.user().profile.schedule[0] : false} value="Sunday" ref="Sunday" />
+                                    <label htmlFor={this.id}> Sunday</label>
+                                    
                                 </div>                        
                             <div className="spacer col-sm-12"/>
                                 <div className="col-sm-12">
                                     <p className="p-override">NAME</p>
-                                    <input className="full-width" type="text" ref="characterName" />
+                                    <input className="full-width" type="text" ref="characterNames" />
                                 </div>                        
                             <div className="spacer col-sm-12"/>
                                 <div className="col-sm-12">
@@ -121,6 +177,9 @@ export default class Settings extends React.Component{
                                 <div className="spacer col-sm-12"/>
 
 
+                            </div>
+                            <div className="col-sm-12">
+                                <button className="full-width submit-button blue-button">SUBMIT CHANGES</button>
                             </div>
                         </form>
                         
