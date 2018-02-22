b11455f7f0da7db9227db2c010f8591e33c335b7
Austin Shipley <austin.r.shipley@gmail.com>
2018-02-22 10:37:35 -0500

    Working on chat

--- imports/components/Mail.js
+++ imports/components/Mail.js
@@ -33,69 +33,52 @@ export default class Mail extends React.Component{
         return <div>{cards}</div>;
     }
 
-    renderChat(){
-        messages = this.conversations
-        for(var i = 0; i < numMsgs; i++)
-        {
-        }
-    }
-
     loadConversation(contactID){
+        if (contactID){
+            return <ChatWindow contactID={contactID}/>
+        }
+        else{
+            return <div/>
+        }
     }
 
   render() {
     return(
         <div className="page-wrapper">
-                <div className="col-lg-8 col-lg-offset-2">
-                    <div className=" col-xs-12 fill-height scrolling-container" >
-
-
-                        <div className="col-lg-4">
-
-                                         
-                            <div className="page-content col-sm-12" style={{"height":"200px"}}>
-                                <p>Find Friends</p>
-                                <input type="text" className="full-width"/>
-                            </div>
-                            <div className="spacer col-sm-12"/>                      
-                            <div className="spacer col-sm-12"/>
-                            <div className="col-sm-12">
-                                <h3>Friend List</h3>
-                                <hr/>
-                                <div className="scrolling-container"  style={{"height":"545px"}}>
-                                    {this.renderPlayers()}
-                                </div>
-                                
-
-
-                            </div>
-
-
-
-                            
+            <div className="col-lg-8 col-lg-offset-2">
+                <div className=" col-xs-12 fill-height scrolling-container" >
+                    <div className="col-lg-4">
+                        <div className="page-content col-sm-12" style={{"height":"200px"}}>
+                            <p>Find Friends</p>
+                            <input type="text" className="full-width"/>
                         </div>
-
-
-                        <div className="col-lg-8">
-                            <div className="page-content col-sm-12" style={{"height":"80px"}}>
-                                <div style={{"float":"left"}}>
-                                    <div style={{ "width":"60px","height":"60px", "backgroundColor":"red"}}/>
-                                </div>
-                                <div style={{"float":"left", "width":"300px","marginLeft":"30px"}}>
-                                    <h4>Chatting With: xXRAWRXx_360NOXSCOPEXDXx</h4>
-                                </div>
-
+                        <div className="spacer col-sm-12"/>                      
+                        <div className="spacer col-sm-12"/>
+                        <div className="col-sm-12">
+                            <h3>Friend List</h3>
+                            <hr/>
+                            <div className="scrolling-container"  style={{"height":"545px"}}>
+                                {this.renderPlayers()}
                             </div>
-                            <div className="spacer col-sm-12"/>
-                            <div className="spacer col-sm-12"/>
-                            <div className="spacer col-sm-12"/>
-
-                            <ChatWindow/>
-
-                        </div> 
+                        </div>
                     </div>
+                    <div className="col-lg-8">
+                        <div className="page-content col-sm-12" style={{"height":"80px"}}>
+                            <div style={{"float":"left"}}>
+                                <div style={{ "width":"60px","height":"60px", "backgroundColor":"red"}}/>
+                            </div>
+                            <div style={{"float":"left", "width":"300px","marginLeft":"30px"}}>
+                                <h4>Chatting With: xXRAWRXx_360NOXSCOPEXDXx</h4>
+                            </div>
+                        </div>
+                        <div className="spacer col-sm-12"/>
+                        <div className="spacer col-sm-12"/>
+                        <div className="spacer col-sm-12"/>
+                        {loadConversation}
+                    </div> 
                 </div>
             </div>
+        </div>
     );
   }
 }  
