fd799cc292a457309313368c4ae6bb9ee660f0c4
Austin Shipley <austin.r.shipley@gmail.com>
2018-02-22 13:47:51 -0500

    Implemented chat system

--- imports/objects/ChatWindow.js
+++ imports/objects/ChatWindow.js
@@ -4,18 +4,28 @@ import UserBubble from '../objects/UserSpeechBubble';
 import OtherBubble from '../objects/OtherSpeechBubble';
 
 export default class ChatWindow extends React.Component{
+    renderChatBubbles(){
+        var cards = [];
+
+        if (this.props.conversation){
+            messages = this.props.conversation.messages;
+            for (i = 0; i < messages.length; i++){
+                if (messages[i].userID == Meteor.userId()){
+                    cards.push(<UserBubble key={i} message={messages[i].message}/>);
+                }
+                else{
+                    cards.push(<OtherBubble key={i} message={messages[i].message}/>);
+                }
+            }
+        }
+
+        return cards;
+    }
     render() {
-        console.log("chatwindow");
-        console.log(this.props.contactID);
         return (
             <div className="">
                 <div className="col-sm-12 full-width scrolling-container-50" >
-                    <UserBubble/>
-                    <OtherBubble/>
-                    <UserBubble/>
-                    <OtherBubble/>
-                    <UserBubble/>
-
+                    {this.renderChatBubbles()}
                 </div>
                 
             </div>
