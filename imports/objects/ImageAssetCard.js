2d662c589d5315d13c7bd423c2e2c7ac4243eaac
Michael Garro <mgarro385@knights.ucf.edu>
2018-03-06 12:33:47 -0500

    Made it such that GM cant accidentally delete assets from CampaignScreen

--- imports/objects/ImageAssetCard.js
+++ imports/objects/ImageAssetCard.js
@@ -19,12 +19,10 @@ export default class ImageAssetCard extends React.Component{
                 <div className="image-asset">
                     <img src={this.props.URL == null || this.props.URL == "" ? '/images/addIcon.png' : this.props.URL} className="image-asset-img" />                    
                 </div>
-                <div className="image-buttons">
-
-                    <button onClick={this.deleteImage.bind(this)} className="tiny-delete-btn">
+                <div className={this.props.isCampaignScreen ? "" : "image-buttons"}>
+                    <button onClick={this.props.isCampaignScreen ? this.nothing() : this.deleteImage.bind(this)} className="tiny-delete-btn">
                         <p  className="tiny-button-txt"><img src={'/images/x-icon.png'} className=""/>  DELETE</p>
                     </button>
-
                 </div>
             </div>
         );
