f1b6dc9aa5faea84f3c6a19faffe358b71542fc5
JKramerDD <jkramerdesigns@hotmail.com>
2018-03-18 13:43:21 -0500

    cs popup

---
+++ imports/objects/CharacterSheetPopup.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import UserCard from '../objects/UserCard';
+
+
+export default class PlayerFormPopup extends React.Component {
+    componentWillMount(){
+        this.playerFormPopupTracker = Tracker.autorun(() => {
+            const sub = Meteor.subscribe('conversations');
+            if(sub.ready())
+            {
+                this.conversations = Conversations.find( {$or: [{"userOne._id": Meteor.userId()}, {"userTwo._id": Meteor.userId()}]}).fetch();
+            }
+
+            const sub2 = Meteor.subscribe('userData');
+            if(sub2.ready())
+            {
+                this.users = Meteor.users.find({}).fetch();
+            }
+
+            this.forceUpdate();
+        });
+    }
+
+    componentWillUnmount(){
+        this.playerFormPopupTracker.stop();
+    }
+
+    renderContacts() {
+        var cards = [];
+        if (this.conversations){
+            for (var i = 0; i < this.conversations.length; i++){
+                partner = (this.conversations[i].userOne._id == Meteor.userId()) ? this.conversations[i].userTwo : this.conversations[i].userOne;
+                if (!this.alreadyInvited(partner)){
+                    cards.push(<UserCard key={i} username={partner.profile.username} accountPicture={partner.profile.accountPicture} func={this.props.addPlayer} param={partner._id}/>);
+                }
+            }
+        }
+        return <div>{cards}</div>;
+    }
+
+    alreadyInvited(player){
+        for (var i = 0; i < this.props.pendingInvites.length; i++){
+            if (this.props.pendingInvites[i] == player._id){
+                return true;
+            }
+        }
+
+        for (var i = 0; i < this.props.characters.length; i++){
+            if (this.props.characters[i].UID == player._id){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    addPlayer(userID){
+        if (!userID){
+            userID = this.refs.username.value;
+        }
+
+        this.props.addPlayer(userID);
+    }
+
+    render() {
+        return (
+            <div className='popup'>
+                <div className="add-player-popup popup_inner">
+                    <h2>Enter Player Username</h2>
+                    <input type="text" ref="username" className="full-width"/>
+                    <div className="col-sm-12">
+                        <div className="right-align">
+                            <button onClick={this.props.closePopup} className=" submit-button button">Cancel</button>
+                            <button onClick={this.addPlayer.bind(this)} className="submit-button blue-button button">Add Player</button>
+                        </div>       
+                        <div className="spacer col-sm-12"/>                      
+                        <div className="spacer col-sm-12"/>
+                        <h4>Or select from Contacts</h4>
+                        <div className="full-height">
+                            <div className="scrolling-container" style={{"height":"250px", "width":"340px"}}>
+                                {this.renderContacts()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
