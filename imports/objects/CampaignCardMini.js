95cf6f0f6c36282f8d5b3d313605dbc426252362
mgarro385 <mgarro385@knights.ucf.edu>
2018-04-05 17:52:29 -0400

    Scheduling algorithm has been written. Not sure what exactly tyo do with the results

--- imports/objects/CampaignCardMini.js
+++ imports/objects/CampaignCardMini.js
@@ -2,7 +2,65 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 
 
+var gmSchedule;
+var characters;
 export default class CampaignCardMini extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+    schedule(e)
+    {
+        if (!e)
+        {
+            var e = window.event;
+        }
+        e.cancelBubble = true;
+        if (e.stopPropagation)
+        {
+            e.stopPropagation();
+        }
+
+        if(this.props.characters == undefined)
+        {
+            console.log("No characters in Campaign");
+            return;
+        }
+
+        var days = [];
+        days.push(false); //Monday
+        days.push(false); //Tuesday
+        days.push(false); //Wednesday
+        days.push(false); //Thursday
+        days.push(false); //Friday
+        days.push(false); //Saturday
+        days.push(false); //Sunday
+
+        var gmSchedule = Meteor.users.findOne({_id: this.props.campaignGM}).profile.schedule;
+        var userSchedule;
+        var available;
+        for(var i = 0; i < days.length; i++)
+        {
+            available = true;
+            if(gmSchedule[i])
+            {
+                for(var j = 0; j < this.props.characters.length; j++)
+                {
+                    userSchedule = Meteor.users.findOne({_id: this.props.characters[j].UID}).profile.schedule;
+                    if(userSchedule[i] == false)
+                    {
+                        available = false;
+                        break;
+                    }
+                }
+                if(available)
+                {
+                    days[i] = true;
+                }
+            }
+        }
+        console.log(days);
+    }
     render() {
         return (
             <NavLink to='#' onClick={() => this.props.func(this.props.id, this.props.somehistory, this.props.campaigns)} className='nav-item nav-link hoverBox'>   
@@ -16,7 +74,7 @@ export default class CampaignCardMini extends React.Component{
                         <p className="p-override no-margin-override small-text"> {this.props.campaignDescription}</p>
                     </div>
                     <div  className=" cal-buttons">
-                         <div className="cal-img stretch-image" draggable="false"/>
+                         <div onClick={this.schedule.bind(this)} className="cal-img stretch-image" draggable="false"/>
                     </div>
                 </div>
             </NavLink>
